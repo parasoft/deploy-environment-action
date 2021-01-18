7dc503023b4e8b7c7e535d98091efae8e2bc8b34
Steve Wang <swang@parasoft.com>
2021-01-18 12:30:52 -0800

    @task CTP-6314 implement simple deploy-environment github action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,22 +3,14 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_CTPURL'] = 'http://104.42.225.105/em'
+  process.env['INPUT_CTPUSERNAME'] = 'admin'
+  process.env['INPUT_CTPPASSWORD'] = 'admin'
+  process.env['INPUT_SYSTEM'] = 'ParaBank'
+  process.env['INPUT_ENVIRONMENT'] = 'Golden'
+  process.env['INPUT_INSTANCE'] = 'Negative'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
