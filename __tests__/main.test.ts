9405ac990f8b3327da0f3860154a8a2cc4f2ae0d
Steve Wang <swang@parasoft.com>
2021-01-28 16:16:46 -0800

    @task CTP-637 mocking rest api calls

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,24 +1,110 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as core from '@actions/core';
+import * as main from '../src/main';
+import * as service from '../src/service';
+import q from 'q';
+import http from 'http';
+
+const systemResponse_SUCCESS = {"systems": [{"id": 10, "name": "fake-system"}]};
+const environmentResponse_SUCCESS = {"environments": [ {"id": 29, "name": "fake-environment", "systemId": 10 }]}
+const instanceResponse_SUCCESS = {"instances": [{"id": 131, "name": "fake-instance"}]};
+const provision_SUCCESS = {
+  "environmentId": 29,
+  "instanceId": 131,
+  "abortOnFailure": true,
+  "steps": [
+    {
+      "percent": 0,
+      "stepId": 0,
+      "description": "string",
+      "name": "string",
+      "result": "SUCCESS"
+    }
+  ],
+  "status": "SUCCESS",
+  "eventId": 0
+}
+const getProvision_SUCCESS = {"eventId": 1, "environmentId": 29, "instanceId": 131, "abortOnFailure": true, "status": "success", "steps": []};
+
+test('test basic deploy environment', () => {
+    jest.spyOn(core, 'getInput').mockImplementation((val) => {
+        if (val === 'ctpUrl') {
+            return 'https://fake-ctp-endpoint:8080/em/'
+        } else if (val === 'ctpUsername' || val === 'ctpPassword') {
+            return 'admin';
+        } else if (val === 'systemName') {
+            return 'fake-system'
+        } else if (val === 'environmentName') {
+          return 'fake-environment'
+        } else if (val === 'instanceName') {
+          return 'fake-instance'
+       }
+    });
+    jest.spyOn(service.WebService.prototype, 'findInEM').mockImplementation((path, property, name) => {
+        let def = q.defer();
+        let promise = new Promise((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        console.log('findInEM invoked');
+        var res = new http.IncomingMessage(null);
+        res.statusCode = 200;
+        if (path === '/api/v2/systems') {
+          def.resolve(systemResponse_SUCCESS);
+        }
+        else if (path === '/api/v2/environments') {
+          def.resolve(environmentResponse_SUCCESS);
+        }
+        else if (path === '/api/v2/environments/29/instances') {
+          def.resolve(instanceResponse_SUCCESS);
+      }
+        return promise;
+    });
+    jest.spyOn(service.WebService.prototype, 'postToEM').mockImplementation((path, data) => {
+        let def = q.defer();
+        let promise = new Promise((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        console.log('postToEM invoked');
+        var res = new http.IncomingMessage(null);
+        res.statusCode = 200;
+        if (path === '/api/v2/provisions') {
+          def.resolve(provision_SUCCESS);
+        }
+        return promise;
+    });
+    jest.spyOn(service.WebService.prototype, 'getFromEM').mockImplementation((path) => {
+      let def = q.defer();
+      let promise = new Promise((resolve, reject) => {
+          def.resolve = resolve;
+          def.reject = reject;
+      });
+      console.log('getFromEM invoked');
+      var res = new http.IncomingMessage(null);
+        res.statusCode = 200;
+        if (path === '/api/v2/provisions/1') {
+          def.resolve(getProvision_SUCCESS);
+        }
+      return promise;
+  });
+    main.run();
+});
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_CTPURL'] = 'http://104.42.225.105/em'
-  process.env['INPUT_CTPUSERNAME'] = 'admin'
-  process.env['INPUT_CTPPASSWORD'] = 'admin'
-  process.env['INPUT_SYSTEM'] = 'ParaBank'
-  process.env['INPUT_ENVIRONMENT'] = 'Golden'
-  process.env['INPUT_INSTANCE'] = 'Negative'
-  process.env['INPUT_COPYTOVIRTUALIZE'] = 'true'
-  process.env['INPUT_VIRTSERVERNAME'] = 'AzureVirtServer'
-  process.env['INPUT_NEWENVIRONMENTNAME'] = 'NewFromAction'
-  process.env['INPUT_DUPLICATEDATAREPO'] = 'true'
-  process.env['INPUT_DUPLICATETYPE'] = 'default'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+// test('test runs', () => {
+//   process.env['INPUT_CTPURL'] = 'http://104.42.225.105/em'
+//   process.env['INPUT_CTPUSERNAME'] = 'admin'
+//   process.env['INPUT_CTPPASSWORD'] = 'admin'
+//   process.env['INPUT_SYSTEM'] = 'ParaBank'
+//   process.env['INPUT_ENVIRONMENT'] = 'Golden'
+//   process.env['INPUT_INSTANCE'] = 'Negative'
+//   const np = process.execPath
+//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//   const options: cp.ExecFileSyncOptions = {
+//     env: process.env
+//   }
+//   console.log(cp.execFileSync(np, [ip], options).toString())
+// })
---
+++ src/service.ts
@@ -0,0 +1,164 @@
+import * as core from '@actions/core'
+import http = require('http');
+import https = require('https');
+import q = require('q');
+import url = require('url');
+
+export interface Authorization {
+    username: string,
+    password: string
+}
+
+export class WebService {
+    private baseURL: url.Url;
+    protected protocol: typeof https | typeof http;
+    protected protocolLabel: string;
+    protected authorization: Authorization;
+
+    constructor(endpoint: string, context: string, authorization?: Authorization) {
+        this.baseURL = url.parse(endpoint);
+        if (this.baseURL.path === '/') {
+            this.baseURL.path += context;
+        } else if (this.baseURL.path === `/${context}/`) {
+            this.baseURL.path = `/${context}`;
+        }
+        this.authorization = authorization;
+        this.protocol = this.baseURL.protocol === 'https:' ? https : http;
+        this.protocolLabel = this.baseURL.protocol || 'http:';
+    }
+
+    getFromEM<T>(path: string) : Promise<T> {
+        let def = q.defer<T>();
+        let promise = new Promise<T>((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        var options = {
+            host: this.baseURL.hostname,
+            port: this.baseURL.port,
+            path: this.baseURL.path + path,
+            auth: undefined,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        if (this.protocolLabel === 'https:') {
+            options['rejectUnauthorized'] = false;
+            options['agent'] = false;
+        }
+        if (this.authorization) {
+            options.auth = this.authorization.username + ':' +  this.authorization.password;
+        }
+        console.log('GET ' + this.protocolLabel + '//' + options.host + ':' + options.port + options.path);
+        var responseString = "";
+        this.protocol.get(options, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseString += chunk;
+            });
+            res.on('end', () => {
+                console.log('    response ' + res.statusCode + ':  ' + responseString);
+                var responseObject = JSON.parse(responseString);
+                def.resolve(responseObject);
+            });
+        }).on('error', (e) => {
+            def.reject(e);
+        });
+        return promise;
+      };
+      
+      findInEM<T>(path: string, property: string, name: string) : Promise<T> {
+        let def = q.defer<T>();
+        let promise = new Promise<T>((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        var options = {
+            host: this.baseURL.hostname,
+            port: this.baseURL.port,
+            path: this.baseURL.path + path,
+            auth: undefined,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        if (this.protocolLabel === 'https:') {
+            options['rejectUnauthorized'] = false;
+            options['agent'] = false;
+        }
+        if (this.authorization) {
+          options.auth = this.authorization.username + ':' +  this.authorization.password;
+        }
+        var responseString = "";
+        console.log('GET ' + this.protocolLabel + '//' + options.host + ':' + options.port + options.path);
+        this.protocol.get(options, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseString += chunk;
+            });
+            res.on('end', () => {
+                console.log('    response ' + res.statusCode + ':  ' + responseString);
+                var responseObject = JSON.parse(responseString);
+                if (typeof responseObject[property] === 'undefined') {
+                    def.reject(property + ' does not exist in response object from ' + path);
+                    return;
+                }
+                for (var i = 0; i < responseObject[property].length; i++) {
+                    if (responseObject[property][i].name === name) {
+                        def.resolve(responseObject[property][i]);
+                        return;
+                    }
+                }
+                def.reject('Could not find name "' + name + '" in ' + property + ' from ' + path);
+                return;
+            });
+        }).on('error', (e) => {
+            def.reject(e);
+        });
+        return promise;
+      };
+      
+      postToEM<T>(path: string, data: any) : Promise<T>{
+        let def = q.defer<T>();
+        let promise = new Promise<T>((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        var options = {
+            host: this.baseURL.hostname,
+            port: parseInt(this.baseURL.port),
+            path: this.baseURL.path + path,
+            method: 'POST',
+            auth: undefined,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+        if (this.protocolLabel === 'https:') {
+            options['rejectUnauthorized'] = false;
+            options['agent'] = false;
+        }
+        if (this.authorization) {
+          options.auth = this.authorization.username + ':' +  this.authorization.password;
+        }
+        console.log('POST ' + this.protocolLabel + '//' + options.host + ':' + options.port + options.path);
+        var responseString = "";
+        var req = this.protocol.request(options, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseString += chunk;
+            });
+            res.on('end', () => {
+                console.log('    response ' + res.statusCode + ':  ' + responseString);
+                var responseObject = JSON.parse(responseString);
+                def.resolve(responseObject);
+            });
+        }).on('error', (e) => {
+            def.reject(e);
+        });
+        req.write(JSON.stringify(data));
+        req.end();
+        return promise;
+      }
+} 
